9db6813c11be96c1c5afbac838ad8ffbd14675c9
ryzhe <37793246+ryzhe@users.noreply.github.com>
2019-07-03 14:34:54 +0700

    Add files via upload

---
+++ WebProject-Supi/persegi-panjang.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Persegi Panjang</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+	<form action="persegi-panjang.php" method="post">
+	<a href="main-menu.php"><img src="back.png" width="40px" height="40px"></a>
+	<a href="index.php"><img src="home.png" width="28px" height="31px"></a>
+	<p>Caption later</p>
+	Width : <input type="text" name="width">
+	Height : <input type="text" name="height">
+	<input type="submit" name="submit" value="Calculate">
+</form>
+</body>
+</html>
+
+<?php  
+
+	if (isset($_POST['submit'])) {
+		$width = $_POST['width'];
+		$height = $_POST['height'];
+		
+		
+		$resultA = $width * $height;
+		$resultC = 2 * ($width + $height);
+
+		echo "Your Area Result is : $resultA";
+		echo "<br>";
+		echo "Your Circumference Result is : $resultC";
+	}
+
+?>
---
+++ WebProject-Supi/segitiga-sisi.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Segitiga Sama Sisi</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+	<form action="segitiga-sisi.php" method="post">
+	<a href="main-menu.php"><img src="back.png" width="40px" height="40px"></a>
+	<a href="index.php"><img src="home.png" width="28px" height="31px"></a>
+	<p>Caption later</p>
+	Side : <input type="text" name="side"><br>
+	Tall : <input type="text" name="tall"><br>
+	<input type="submit" name="submit" value="Calculate">
+</form>
+</body>
+</html>
+
+<?php  
+
+	if (isset($_POST['submit'])) {
+		$side = $_POST['side'];
+		$tall = $_POST['tall'];
+		
+		$resultA = ($side * $tall) / 2;
+		$resultC = $side + $side + $side;
+
+		echo "Your Area Result is : $resultA";
+		echo "<br>";
+		echo "Your Circumference Result is : $resultC";
+	}
+
+?>
